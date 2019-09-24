fae1f5f53c558721f8068bcb5fc1a4ec855c68e4
Bishop Bader <bflatbader@gmail.com>
2019-09-23 21:05:54 -0400

    added logic to calculate train times, edited styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,44 @@
 // FUNCTIONS
+function tableUpdate () {
+    // Discard old information
+    $('#trainTable').empty();
+    
+    // Grab information from database
+    database.ref().on("child_added", function(snapshot) {
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
+        console.log(sv.trainName, sv.trainDestination, sv.trainStart, sv.trainFrequency);
+
+        var timeConvert = moment(sv.trainStart, "HHmm").subtract(1, "years");
+        var timeDifference = moment().diff(moment(timeConvert), "minutes");
+        var timeRemaining = timeDifference % sv.trainFrequency;
+        var timeAway = sv.trainFrequency - timeRemaining;
+        var nextArrival = moment().add(timeAway, "minutes");
+        var arrivalDisplay = moment(nextArrival).format("HH:mm");
+
+        // Change the HTML to reflect
+        trainInfo = `
+        <tr>
+        <td>${sv.trainName}</td>
+        <td>${sv.trainDestination}</td>
+        <td>${sv.trainFrequency}</td>
+        <td>${arrivalDisplay}</td>
+        <td>${timeAway}</td>
+        </tr>
+        `;
+
+        $('#trainTable').append(trainInfo);
+        
+        // Handle the errors
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+    });
+}
+
 function timeUpdate () {
     currentTime = moment();
     $("#clock").text(currentTime.format("HH:mm:ss A"));
+    tableUpdate();
 }
 
 // VARIABLES
@@ -57,26 +94,5 @@ $('#addTrain').click(function(){
 $("#clock").text(currentTime.format("HH:mm:ss A"));
 setInterval(timeUpdate, 1000);
 
-// Grab information from database
-database.ref().on("child_added", function(snapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = snapshot.val();
-    console.log(sv.trainName, sv.trainDestination, sv.trainStart, sv.trainFrequency);
-
-    // Change the HTML to reflect
-    trainInfo = `
-    <tr>
-    <td>${sv.trainName}</td>
-    <td>${sv.trainDestination}</td>
-    <td>${sv.trainFrequency}</td>
-    <td></td>
-    <td></td>
-    </tr>
-    `;
-
-    $('#trainTable').append(trainInfo);
-    
-    // Handle the errors
-    }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-});
+// load train schedule table
+tableUpdate();
